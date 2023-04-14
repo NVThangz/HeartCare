acda6b85e6246826de812c85c2210c260718fe82
Nguyen Chau Khanh <chaukhanh0605@gmail.com>
2023-04-14 13:12:49 +0700

    Initial front-end setup and add login interface

---
+++ frontend/app/src/main/java/com/example/heartcare/SignIn.java
@@ -0,0 +1,14 @@
+package com.example.heartcare;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignIn extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+
+}

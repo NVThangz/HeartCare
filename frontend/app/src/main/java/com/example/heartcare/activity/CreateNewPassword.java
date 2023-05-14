a105762f05e1a33c9d612c24af590358dfc96a63
Nguyen Chau Khanh <chaukhanh0605@gmail.com>
2023-05-14 19:00:35 +0700

    Add Create New Password interface

--- frontend/app/src/main/java/com/example/heartcare/activity/CreateNewPassword.java
+++ frontend/app/src/main/java/com/example/heartcare/activity/CreateNewPassword.java
@@ -1,54 +1,52 @@
 package com.example.heartcare.activity;
 
-import android.accessibilityservice.InputMethod;
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.renderscript.ScriptGroup;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.chaos.view.PinView;
 import com.example.heartcare.R;
 
-public class VerifyEmail extends AppCompatActivity {
+public class CreateNewPassword extends AppCompatActivity {
     private ImageView icBack;
-    private TextView btnVerify;
-    private PinView pinView;
+    private TextView btnSend;
+    private EditText etPassword;
+    private EditText etConfirmPassword;
+
     private void map() {
         icBack = findViewById(R.id.ic_back);
-        btnVerify = findViewById(R.id.btn_verify);
-        pinView = findViewById(R.id.pin_view);
+        btnSend = findViewById(R.id.btn_save);
+        etPassword = findViewById(R.id.et_password);
+        etConfirmPassword = findViewById(R.id.et_confirm_password);
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_verify_email);
+        setContentView(R.layout.activity_create_new_password);
         map();
-
         setFocusChangeListener();
-        setPinView();
         clickIcBack();
         clickBtnSend();
     }
 
-    public void hideKeyboard(View view) {
-        InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
-        inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
-    }
-
     private void setFocusChangeListener() {
-        pinView.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+        etPassword.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (!hasFocus) {
+                    hideKeyboard(v);
+                }
+            }
+        });
+
+        etConfirmPassword.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
             public void onFocusChange(View v, boolean hasFocus) {
                 if (!hasFocus) {
@@ -58,17 +56,16 @@ public class VerifyEmail extends AppCompatActivity {
         });
     }
 
-    private void setPinView() {
-        pinView.requestFocus();
-        InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-        inputMethodManager.toggleSoftInput(InputMethodManager.SHOW_FORCED, InputMethodManager.HIDE_IMPLICIT_ONLY);
+    public void hideKeyboard(View view) {
+        InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 
     private void clickBtnSend() {
-        btnVerify.setOnClickListener(new View.OnClickListener() {
+        btnSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(VerifyEmail.this, CreateNewPassword.class);
+                Intent intent = new Intent(CreateNewPassword.this, MainActivity.class);
                 startActivity(intent);
             }
         });
